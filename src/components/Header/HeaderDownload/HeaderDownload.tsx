9f22a78675b9c7f63fb38dbde305f7a5681f9cdc
AxiixAAA <slavae22@mail.ru>
2022-08-22 09:26:19 +0500

    starttt

--- src/components/Header/HeaderDownload/HeaderDownload.tsx
+++ src/components/Header/HeaderDownload/HeaderDownload.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "@mui/material";
 
 const HeaderDownload = () => {
-  return <Button variant="contained">Сохранить</Button>;
+  return <Button variant="contained">Сохранdddить</Button>;
 };
 
 export default HeaderDownload;
